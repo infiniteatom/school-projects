03a2ec5a3d61802d5cbaaf62cb15a4aac0cbcaf2
infiniteatom <infinite_atom@yahoo.com>
2016-04-11 12:26:07 +0300

    Added files via upload

--- sudoku.c
+++ sudoku.c
@@ -16,17 +16,65 @@ int     isAvailable(int row, int col, int num)
     return (1);
 }
 
-void    backTracking(int row)
+int     backTracking(int row, int col)
 {
+    if(row < 9 && col < 9)
+    {
+        if(board[row][col] != 0)
+        {
+            if((col + 1) < 9)
+                return backTracking(row, col + 1);
+            else if ((row + 1) < 9)
+                return backTracking(row + 1, 0);
+            else return 1;
+        }
+        else
+        {
+            for(int i = 1; i <= 9; i++)
+            {
+                if(isAvailable(row, col, i))
+                {
+                    board[row][col] = i;
+                    if ((col + 1) < 9)
+                    {
+                        if (backTracking(row, col + 1))
+                            return 1;
+                        else
+                            board[row][col] = 0;
+                    }
+                    else if((row + 1) < 9)
+                    {
+                        if(backTracking(row + 1, 0))
+                            return 1;
+                        else
+                            board[row][col] = 0;
+                    }
+                    else return 1;
+                }
+            }
+        }
+        return 0;
+    }
+    else return 1;
 }
 
 void    printSudoku()
 {
+    printf("+- - -+- - -+- - -+\n");
     for (int i = 0; i < 9; i++)
     {
+        printf("|");
         for (int j = 0; j < 9; j++)
+        {
             printf("%d", board[i][j]);
+            if (j % 3 != 2)
+                printf(" ");
+            if ((j + 1) % 3 == 0)
+                printf("|");
+        }
         printf("\n");
+        if ((i + 1) % 3 == 0)
+            printf("+- - -+- - -+- - -+\n");
     }
 }
 
@@ -44,6 +92,9 @@ void    scanFile(const char file[])
 int     main(void)
 {
     scanFile("sudoku.txt");
-    printSudoku();
+    if (backTracking(0, 0))
+        printSudoku();
+    else
+        printf("No solutions");
     return (0);
 }
